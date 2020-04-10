2d228c6555b9529b71dfe99e21b3497ac3fb2b0f
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-04-10 23:38:58 +0000

    Fix invalid class for trait in DateDeleteAwareTrait

--- src/DateDeletedAwareTrait.php
+++ src/DateDeletedAwareTrait.php
@@ -8,7 +8,7 @@ namespace Arp\Entity;
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
  * @package Arp\Entity
  */
-class DateDeletedAwareTrait
+trait DateDeletedAwareTrait
 {
     /**
      * @var \DateTime|null
