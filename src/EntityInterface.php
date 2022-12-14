8615ec95484b2dab192f9e457e5be19ecb7abbdc
Alex Patterson <alex.patterson.webdev@gmail.com>
2022-12-14 18:01:18 +0000

    Update EntityInterface and EntityTrait to use integer ID property type hints

--- src/EntityInterface.php
+++ src/EntityInterface.php
@@ -4,43 +4,13 @@ declare(strict_types=1);
 
 namespace Arp\Entity;
 
-/**
- * Generic interface for Entity data transfer object.
- *
- * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
- * @package Arp\Entity
- */
 interface EntityInterface
 {
-    /**
-     * Return the entity's identity.
-     *
-     * @return string|null
-     */
-    public function getId(): ?string;
+    public function getId(): ?int;
 
-    /**
-     * Set the entity's identity.
-     *
-     * @param string|null $id The identity that should be set
-     *
-     * @return void
-     */
-    public function setId(?string $id): void;
+    public function setId(?int $id): void;
 
-    /**
-     * Check if the identity has been set.
-     *
-     * @return boolean
-     */
     public function hasId(): bool;
 
-    /**
-     * Check if the provided id matches the $id provided.
-     *
-     * @param string $id The identity that should be compared.
-     *
-     * @return boolean
-     */
-    public function isId(string $id): bool;
+    public function isId(int $id): bool;
 }
